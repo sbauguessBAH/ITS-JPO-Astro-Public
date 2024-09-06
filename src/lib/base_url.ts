b4f29f76d5f33dca5f4f7d9673556126c2a8a409
Young, Caden <Caden.Young@noblis.org>
2024-09-06 14:09:41 -0400

    updated nav URLs to use new base

--- src/lib/base_url.ts
+++ src/lib/base_url.ts
@@ -1,3 +1,8 @@
-export default function base_url(relative_path: string): string {
-  return import.meta.env.BASE_URL + relative_path;
+/**
+ * Takes in an absolute path (for a href, link href, public directory, etc)
+ * @param abs_path 
+ * @returns the absolute path with the base/subdirectory added
+ */
+export default function base_url(abs_path: string): string {
+  return import.meta.env.BASE_URL + abs_path;
 }
