2985c778f27d302d052691a326aafd24d6343495
Bauguess, Shawn [USA] <Bauguess_Shawn@bah.com>
2025-03-21 11:01:57 -0400

    Files changed to remove search indexing
    This is the version that I've provided to ITS JPO to review content to start being pulled back into the website.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   // If hosting on a subdomain, set the base here such as /itsjpo, /demo, etc. 
-  base: 'itstest_environment',
+  base: 'ITSJPO_New-Design_Archive',
   output: 'static',
 });
 
