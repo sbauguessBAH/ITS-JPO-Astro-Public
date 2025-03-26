8666a54861e8e90448dc273b0dc1af8d774b0dac
mhealyBah <healy_michael@bah.com>
2025-03-26 12:57:56 -0400

    Integrated astrojs/sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,13 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-
-import partytown from '@astrojs/partytown';
-
+import partytown from "@astrojs/partytown";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  // If hosting on a subdomain, set the base here such as /itsjpo, /demo, etc. 
-  base: '',
-
-  output: 'static',
-  integrations: [partytown()],
+  // If hosting on a subdomain, set the base here such as /itsjpo, /demo, etc.
+  base: "",
+  site: "https://www.its.dot.gov",
+  output: "static",
+  integrations: [partytown(), sitemap()],
 });
