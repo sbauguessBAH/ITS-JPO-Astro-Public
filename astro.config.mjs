3708b69a0d949dde20687faebad5b4fdc986499b
mhealyBah <healy_michael@bah.com>
2025-09-08 16:47:50 -0400

    Add more redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,6 +11,14 @@ export default defineConfig({
   output: "static",
   integrations: [partytown(), sitemap()],
   redirects: {
-    "its.dot.gov/index.htm": "its.dot.gov"
-  }
+    "https://its.dot.gov/index.htm": "https://its.dot.gov",
+    "https://its.dot.gov/about/its_jpo.htm": "https://its.dot.gov/about",
+    "https://its.dot.gov/research_areas/automation/index.htm": "https://its.dot.gov/research-areas/Automation-Program",
+    "https://its.dot.gov/contacts/stafflisting.htm": "https://its.dot.gov/about/contact-us/staff-listing/",
+    "https://its.dot.gov/its4us/index.htm": "https://its.dot.gov/research-areas/ITS4US/",
+    "https://its.dot.gov/press/news.htm": "https://www.transportation.gov/newsroom",
+    "https://its.dot.gov/itsjpo_Old-design_archive/communications/its_images.htm":
+      "https://its.dot.gov/news-info/image-library/",
+    
+  },
 });
