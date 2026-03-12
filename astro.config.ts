2c181eb8bb4a2cd69f958d6bd9b15fa6c7d52171
Young, Caden <Caden.Young@noblis.org>
2026-03-12 13:30:51 -0400

    feat: add env var loading for BASE_URL
    This allows you to set a different base in the Astro config based on your environment.

--- astro.config.ts
+++ astro.config.ts
@@ -2,11 +2,15 @@ import partytown from "@astrojs/partytown";
 import sitemap from "@astrojs/sitemap";
 import vue from "@astrojs/vue";
 import { defineConfig } from "astro/config";
+import { loadEnv } from "vite";
+
+/** Load the BASE_URL from a `.env` file */
+const { BASE_URL } = loadEnv(process.env.NODE_ENV ?? "", process.cwd());
 
 // https://astro.build/config
 export default defineConfig({
   // If hosting on a subdomain, set the base here such as /itsjpo, /demo, etc.
-  base: "itstest_programs",
+  base: BASE_URL,
   integrations: [
     partytown(), 
     sitemap(), 
