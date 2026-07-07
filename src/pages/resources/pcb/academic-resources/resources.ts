c91d000c80573d4065c49f4d471af018988dcffe
Young, Caden <Caden.Young@noblis.org>
2026-07-07 17:46:35 -0400

    feat(content): reorganize content library and add new images
    Add images for CAVe and academic resources.
    Rename and reorganize images.
    Remove duplicate images.

---
+++ src/pages/resources/pcb/academic-resources/resources.ts
@@ -0,0 +1,37 @@
+export const academicResources = [
+  {
+    description:
+      "ITS Academic Course Modules aim to advance knowledge of ITS by providing instructor and student content targeted to college and university students. Content includes Instructor Guides, PowerPoint Presentations, Student Participant Materials, and more.",
+    // image: "pcb/course-modules",
+    image: "pcb/case-studies",
+    slug: "course-modules",
+    title: "Academic Course Modules",
+  },
+  {
+    description:
+      "The Transportation Technology Tournament (TTT) is an annual student competition conducted by the USDOT, NOCoE, and ITE allowing students to work with public agencies to apply ITS and TSMO solutions to real-world transportation challenges.",
+    image: "pcb/ttt-banner",
+    slug: "student-competitions",
+    title: "Student Competitions",
+  },
+  // {
+  //   title: "Academic Workshops",
+  //   description: "ITS Academic Workshops provide educators and representatives from public and private entities to explore strategies for integrating ITS education into curricula and provide input for advancing the ITS workforce.",
+  //   image: "pcb/academic-workshops",
+  //   slug: "workshops"
+  // },
+  {
+    description:
+      "The ITS Academic Cohort brings together teaching practitioners and the ITS PCB Program to jointly develop academic resources.",
+    image: "pcb/academic-workshops",
+    slug: "academic-cohort",
+    title: "Academic Cohort",
+  },
+  {
+    description:
+      "ITS Career Paths provides a snapshot overview of ITS careers including key details on specific roles, responsibilities, skills/education, and example career paths",
+    image: "pcb/career-paths",
+    slug: "career-paths",
+    title: "Career Paths",
+  },
+];
