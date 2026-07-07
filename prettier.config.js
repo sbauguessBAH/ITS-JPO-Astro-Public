4e6862e89b4872816a91b65487864ccecca575c0
Young, Caden <Caden.Young@noblis.org>
2026-07-07 15:12:26 -0400

    chore(deps): configure VS Code settings for file formatting
    Configure Prettier to format files on save
    Configure ESLint to run on file save
    Add extension recommendations for code analysis

---
+++ prettier.config.js
@@ -0,0 +1,17 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  plugins: ["prettier-plugin-astro", "prettier-plugin-astro-organize-imports"],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+};
+
+export default config;
