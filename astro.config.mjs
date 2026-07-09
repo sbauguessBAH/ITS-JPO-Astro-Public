e8d27c893655a1a52053befc8b9a88b57eae660f
Young, Caden <Caden.Young@noblis.org>
2026-07-09 12:53:48 -0400

    fix: simplify env var resolution until a real fix is found

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,11 @@ import partytown from "@astrojs/partytown";
 import sitemap from "@astrojs/sitemap";
 import vue from "@astrojs/vue";
 import { defineConfig } from "astro/config";
-import { loadEnv } from "vite";
-
-/** Load the BASE_URL from a `.env` file */
-const { BASE_URL } = loadEnv(process.env.NODE_ENV ?? "", process.cwd());
 
 // https://astro.build/config
 export default defineConfig({
   // If hosting on a subdomain, set the base here such as /itsjpo, /demo, etc.
-  base: BASE_URL,
+  base: "/its-jpo",
   integrations: [partytown(), sitemap(), vue()],
   // site: "https://www.its.dot.gov",
   output: "static",
