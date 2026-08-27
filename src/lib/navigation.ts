2f6d103997aaa3f48bc009897e89f582e2a70653
Nealis, Blue [USA] <Nealis_Christina@bah.com>
2026-08-27 14:21:31 -0400

    Remove outreach resources from submenu

--- src/lib/navigation.ts
+++ src/lib/navigation.ts
@@ -43,7 +43,6 @@ export const navigation: NavigationItem[] = [
           { name: "ITS JPO NOW Newsletter", url: "/news-outreach-tools/newsroom/newsletter-archive" },
           { name: "In the News", url: "/news-outreach-tools/newsroom/in-the-news" },
           { name: "Media Kits", url: "/news-outreach-tools/newsroom/media-kits" },
-          { name: "Outreach Resources", url: "/news-outreach-tools/newsroom/outreach-resources" },
         ],
       },
       {
